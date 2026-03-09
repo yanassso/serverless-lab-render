b0b8dc298d9e33755034bb0c36e319f2e4644f4e
Ваше Имя <@email.com>
2026-03-10 02:08:19 +0300

    Fix merge conflict: remove Git markers

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, request, jsonify
 import os
 
@@ -30,7 +29,6 @@ def echo():
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
-=======
 
 from flask import Flask
 
@@ -42,4 +40,4 @@ def hello():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
->>>>>>> 6fa3a086e9e84fd7766f0cb74cdcf061373f2a6f
+
