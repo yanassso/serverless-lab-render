dc88cade76d87e85295f750be64fca8d1936ed80
Яна <152487864+yanassso@users.noreply.github.com>
2026-03-10 00:21:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
 @app.route('/')
 def hello():
-	return "Hello, Serverless! ??\n", 200, {'Content-Type': 'text/plain'}
+    return "Hello, Serverless! 🚀"
+
+if __name__ != '__main__':
+    # Это нужно для production-серверов
+    pass
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
